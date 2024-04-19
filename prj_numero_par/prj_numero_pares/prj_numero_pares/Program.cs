329a905224ce3086bd2cf118dd064b2cc61ab3f2
Yasmin Tavares <134327320+yaasmintavares@users.noreply.github.com>
2024-04-19 19:02:29 -0300

    Add files via upload
    Exibindo os números PARES de um intervalo de  até 50

---
+++ prj_numero_par/prj_numero_pares/prj_numero_pares/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj_numero_pares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+            for (c = 1; c <= 10; c++)
+            {
+                if (c % 1 == 0)
+                    Console.WriteLine(c + "\n");
+            }
+            Console.WriteLine("\nPress.<ENTER> para finalizar\n");
+            Console.ReadKey();
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
